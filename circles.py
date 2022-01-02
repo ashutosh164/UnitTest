fd98d61465b50fe06c015534078c0680f38f0471
Ashutosh Pradhan <ashutoshpradhan164@gmail.com>
2022-01-02 12:53:23 +0530

    remove extra comment

--- circles.py
+++ circles.py
@@ -7,13 +7,7 @@ def circle_area(r):
     return pi*(r**2)
 
 
-# radii = [2,0,-3,2+3j,True,'radius']
-# message = 'Area of circles with r = {radius} is {area}'
-#
-# for r in radii:
-#     a = circle_area(r)
-#     # print(message.format(radius=r, area=a))
-#     print(f'Area of circles with r = {r} is {a}')
+
 
 
 
